9217479b3d9805311856c8da157e1e2962928abd
M4tisPto <matis.pto.104250@gmail.com>
2025-08-18 14:32:12 -0400

    REACER TODO DE VUELTA

---
+++ script.js
@@ -0,0 +1,5 @@
+/**
+ * Hacer que en la imagen del HRML al pasar el mause a una de las personas, el mause cambie de icono
+ * y al darle click a cualquier persona se abra una ventana emergente con la foto de esa persona.
+ */
+
